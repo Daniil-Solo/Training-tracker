82f3e2705984e7dd8433a1a82473cfc61aecceca
Daniil-Solo <daniil.solo1723@gmail.com>
2021-06-21 19:04:18 +0300

    add naming for interface, add function for close this window and move

--- Program/My_records_window.py
+++ Program/My_records_window.py
@@ -8,10 +8,35 @@ from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow
 
 
 class ViewMyRecords(QMainWindow):
-    def __init__(self):
+    def __init__(self, home_page):
         super(ViewMyRecords, self).__init__()
+        self.home_page = home_page
         loadUi("new_design/my_records.ui", self)
 
+        self.setWindowFlags(QtCore.Qt.FramelessWindowHint)
+        self.setAttribute(QtCore.Qt.WA_TranslucentBackground)
+        self.all_connections()
+
+    def all_connections(self):
+        # закрытие окна
+        self.close.clicked.connect(lambda: self.close_goal_window())
+        # передвижение окна
+        self.header_frame.mouseMoveEvent = self.moveWindow
+
+    def close_goal_window(self):
+        self.home_page.update()
+        self.hide()
+
+
+    def moveWindow(self, e):
+        if not self.isMaximized():
+            if e.buttons() == Qt.LeftButton:
+                self.move(self.pos() + e.globalPos() - self.clickPosition)
+                self.clickPosition = e.globalPos()
+                e.accept()
+
+    def mousePressEvent(self, event):
+        self.clickPosition = event.globalPos()
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
